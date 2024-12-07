66456714959a6dffc39a581a30ccbbcf0ea11d46
hmdftr <hmdftr@gmail.com>
2024-12-07 23:28:30 +0800

    Add projectile script
    nothing inside

---
+++ Source/RPGSystems/Public/AbilitySystem/Abilities/ProjectileAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/RPGGameplayAbility.h"
+#include "ProjectileAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGSYSTEMS_API UProjectileAbility : public URPGGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
